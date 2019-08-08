2049770b1c12fbc1a9f60c611822dbe06f752edc
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-08 22:58:23 +0700

    tugas_22_js
    tugas sudah ok !!!

---
+++ tugas-22.js
@@ -0,0 +1,10 @@
+function panggilforeach(){
+
+  var data = ["Saya", "ingin", "belajar", "bersama"];
+
+    data.forEach(function(item, index){
+    console.log("Item :",item, "Index ke", index);
+
+  })
+}
+panggilforeach();
